88d9cb8682c2e2f8560032163a35653e2f5bfedf
cfycyf <cfycyf@yeah.net>
2019-07-01 20:26:13 +0800

    编写最简单的内核模块hello.ko并加载
    Signed-off-by: cfycyf <cfycyf@yeah.net>

---
+++ kmod/hello.c
@@ -0,0 +1,32 @@
+/*
+ * 在console下可以:
+ * 查看模块信息: modinfo hello.ko
+ * 加载模块: lsmod hello.ko
+ * 卸载模块: rmmod hello.ko
+ * 查看printk输出的信息: tail /var/log/kern.log
+ */
+#include <linux/module.h>     /* 模块头文件，必不可少 */
+#include <linux/kernel.h>     /* KERN_INFO在这里 */
+#include <linux/init.h>       /* 使用的宏 */
+
+MODULE_LICENSE("GPL");
+
+MODULE_AUTHOR("cfycyf");
+
+MODULE_DESCRIPTION("My First LKM hello!");
+
+MODULE_VERSION("4.15");
+
+static int __init hello_init(void)
+{
+	printk(KERN_INFO "Init Hello World\n");
+	return 0;
+}
+ 
+static void __exit hello_exit(void)
+{
+	printk(KERN_INFO "Exit to Hello\n");
+}
+ 
+module_init(hello_init);
+module_exit(hello_exit);
